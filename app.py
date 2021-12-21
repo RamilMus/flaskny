351eab0cbc66d8c4cd7fa8ecd25fe27cac606268
RamilMus <94644460+RamilMus@users.noreply.github.com>
2021-12-21 15:46:19 +0300

    Update app.py

--- app.py
+++ app.py
@@ -62,4 +62,4 @@ def create_article():
         return render_template("create-article.html")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
